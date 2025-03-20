e27d999ae9c6f8cb8fed8bf69a9a43332eaacc5b
Amanda Cornelsen <116924766+AmandaCornelsen@users.noreply.github.com>
2025-03-19 22:12:33 -0300

    Update partida.ts

--- exercicios/partida.ts
+++ exercicios/partida.ts
@@ -1,32 +1,99 @@
-var formPartida = document.getElementById("formPartida") as HTMLFormElement;
-var tbPartidas = document.getElementById("tbPartidas") as HTMLElement;
+//Variáveis globais
+var formCampeonato = document.getElementById(
+  "formCampeonato"
+) as HTMLFormElement;
+var tabelaCampeonato = document.getElementById("tbCampeonatos") as HTMLElement;
+var campeonatos = JSON.parse(localStorage.getItem("campeonatos") || "[]");
 
-interface Partida {
-    timeMandante : string;
-    timeVisitante : string;
+interface Campeonato {
+  id: number;
+  nome: string;
+  categoria: string;
+  tipo: string;
+  dataInicio: string;
+  dataFim: string;
 }
 
-function atualizarTabelaPartida() {
-    tabelaCampeonato.innerHTML = "";
-    formPartida.forEach(( c : Partida )  =>{
-      tbPartidas.innerHTML += `
-      <tr>
-           <td>${c.timeMandante}</td>
-           <td>${c.timeVisitante}</td>
-      </tr>
-    `;
-    })
-  }
+function atualizarTabela() {
+  tabelaCampeonato.innerHTML = "";
+  campeonatos.forEach(( c : Campeonato )  =>{
+    tabelaCampeonato.innerHTML += `
+    <tr>
+         <td>${c.nome}</td>
+         <td>${c.categoria}</td>
+         <td>${c.tipo}</td>
+         <td>${c.dataInicio}</td>
+         <td>${c.dataFim}</td>
+         <td>
+          <button onclick="editarCampeonato(${c.id})"> Editar </button>
+          <button onclick="removerCampeonato(${c.id})"> Remover </button>
+         </td> 
+         </tr>
+  `;
+  })
+}
 
-  function salvarPartida(event:Event) {
-    event?.preventDefault(); //cancelar o disparo do evento
-    const novaPartida: Partida = {
-      timeMandante: (document.getElementById("timeMandante") as HTMLInputElement).value,
-      timeVisitante: (document.getElementById("timeVisitante") as HTMLSelectElement).value 
-    };
-    campeonatos.push(novaPartida)
-    atualizarTabela()
+function editarCampeonato(id:number){
+  //Find = buscar um elemento em um array
+  const campeonato = campeonatos.find(
+    (c : Campeonato) => c.id == id);
+
+    if(!campeonato) return;
+    (document.getElementById("nome") as HTMLInputElement).value = campeonato.nome;
+    (document.getElementById("categoria") as HTMLSelectElement).value = campeonato.categoria;
+    (document.getElementById("tipo") as HTMLSelectElement).value = campeonato.tipo;
+    (document.getElementById("dataInicio") as HTMLInputElement).value = campeonato.dataInicio;
+    (document.getElementById("dataFim") as HTMLInputElement).value = campeonato.dataFim;
+
+    //findIndex buscar o index do objeto
+    const campIndex = campeonatos.findIndex (
+      (c:Campeonato) => c.id == id );
+    
+      //Validar se encontrou algum item
+      if(campIndex !== -1) {
+        //Remover da lista
+        campeonatos.splice(campIndex, 1)
+      }
+
+      salvarLocalStorage()
+      atualizarTabela()
+}
+
+function removerCampeonato(id:number) {
+  const campIndex = campeonatos.findIndex (
+    (c:Campeonato) => c.id == id );
+  
+    //Validar se encontrou algum item
+    if(campIndex !== -1) {
+      //Remover da lista
+      campeonatos.splice(campIndex, 1)
+    }
+
     salvarLocalStorage()
-    formCampeonato.reset()
-    alert('Cadastro com sucesso!')
-  }
+    atualizarTabela()
+}
+
+function salvarLocalStorage() {
+  let campeonatosSalvar = JSON.stringify(campeonatos);
+  localStorage.setItem("campeonatos", campeonatosSalvar);
+}
+
+function salvar(event:Event) {
+  event?.preventDefault(); //cancelar o disparo do evento
+  const novoCampeonato: Campeonato = {
+    id: Date.now(),
+    categoria: (document.getElementById("categoria") as HTMLSelectElement).value,
+    dataFim: (document.getElementById("dataFim") as HTMLInputElement).value,
+    dataInicio:(document.getElementById("dataInicio") as HTMLInputElement).value,
+    nome: (document.getElementById("nome") as HTMLInputElement).value,
+    tipo: (document.getElementById("tipo") as HTMLSelectElement).value 
+  };
+  campeonatos.push(novoCampeonato)
+  atualizarTabela()
+  salvarLocalStorage()
+  formCampeonato.reset()
+  alert('Cadastro com sucesso!')
+}
+
+formCampeonato.addEventListener("submit", salvar)
+atualizarTabela()
